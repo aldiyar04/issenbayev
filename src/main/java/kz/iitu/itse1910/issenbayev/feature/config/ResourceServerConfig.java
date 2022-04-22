6d21b3087bea1e4590bb2fcd0f357bbaf544dbf1
Aldiyar <splitter4774@gmail.com>
2022-04-22 09:46:42 +0600

    OAUTH2 WORKS

--- src/main/java/kz/iitu/itse1910/issenbayev/feature/config/ResourceServerConfig.java
+++ src/main/java/kz/iitu/itse1910/issenbayev/feature/config/ResourceServerConfig.java
@@ -5,15 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.jwk.JwkTokenStore;
-import org.springframework.security.web.AuthenticationEntryPoint;
 
 @Configuration
-@EnableAuthorizationServer
+@EnableResourceServer
 @EnableGlobalMethodSecurity(jsr250Enabled = true, prePostEnabled = true)
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Value("${oauth2.claim.aud}")
